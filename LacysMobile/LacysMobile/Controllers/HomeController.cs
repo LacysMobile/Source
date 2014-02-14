63dad7ea73c2e91877df35b7834b66155d7e15e8
jaxonpickett <jaxon.pickett@gmail.com>
2014-02-14 11:47:38 -0500

    Changed body text
    Changed body text in home/index.html to read "Lacy's Mobile" instead of
    "ASP.NET MVC"

--- LacysMobile/LacysMobile/Controllers/HomeController.cs
+++ LacysMobile/LacysMobile/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace LacysMobile.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Modify this template to jump-start your Lacys Mobile application.";
 
             return View();
         }
